d9281d36c9b672d8f3b43e02fe1e4031e55d1a92
unknown <abhinay07092002@gmail.com>
2021-06-26 09:48:46 +0530

    initial commit

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express =require ('express')
 var app = express(); 
 var server = require('http').Server(app); 
 var io= require('socket.io')(server);
+var port = Process.env.PORT || 3000
 app.use(express.static('public'))
 app.set('view engine','ejs');
 
@@ -16,4 +17,4 @@ io.on('connection', function(socket){
 
 });
 
-server.listen(3000);
+server.listen(port);
