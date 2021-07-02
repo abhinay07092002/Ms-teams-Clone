4ec64f5ad4890af271a8b66e3436f4bbfdbc511c
unknown <abhinay07092002@gmail.com>
2021-07-02 13:00:48 +0530

    initial commit

--- public/client.js
+++ public/client.js
@@ -1,12 +1,20 @@
-var socket = io(); 
-var message = document.getElementById('message');
-var button =  document.getElementById('send');
-var output = document.getElementById('receivedMessages');
-var videoOnOff=document.getElementById('videoOnOff');
-var conn;
-var peer_id=-1;
-var peer = new Peer();
+var socket = io(); //socket.io got intact here
 
+var message = document.getElementById('message');//place of message accessed through DOM
+
+var button =  document.getElementById('send');//send button accessed through DOM
+
+var output = document.getElementById('receivedMessages');//div to store messages accessed through DOM
+
+var videoOnOff=document.getElementById('videoOnOff'); //handling switching on and switching off the audio
+
+var conn;//variable for peer connection
+
+var peer_id=-1;//our peer_id initialized to -1 so that we cannot send message until we are connected to a peer
+
+var peer = new Peer();//dynamic initialization of peer
+
+//when send button is pressed this below segment of code gets activated and perform sending of message to server side from client side 
 button.addEventListener('click', function(){
     if(peer_id!=-1) output.innerHTML += '<p> <strong>' + "You :" +  '</strong>' + message.value + '</p>';
      
@@ -14,15 +22,21 @@ button.addEventListener('click', function(){
         message: message.value,
     })
 }) 
+
+//when receiving a message from server side ,perform the followong action
 socket.on('chat', function(data){
     if(peer_id!=-1) output.innerHTML += '<p> <strong>' + "Person 2" + ': </strong>' + data.message + '</p>';
 })
 
+//Taking our video and audio permissions 
 var constraints={
     audio:true,
     video:true
 }
-let myVideoStream
+
+let myVideoStream //a global variable to keep our stream
+
+//accessing our media and diaplaying it on the screen
 navigator.mediaDevices.getUserMedia(constraints)
 .then(function(stream) {
 
@@ -39,6 +53,8 @@ navigator.mediaDevices.getUserMedia(constraints)
      
 });
 
+
+//our screen sharing permissions
 var displayMediaOptions = {
     video: {
         cursor: "always"
@@ -46,6 +62,7 @@ var displayMediaOptions = {
     audio: false
 };
 
+//displaying our screen and so on and so forth
 document.getElementById('screenShare').addEventListener('click',function(){
     navigator.mediaDevices.getDisplayMedia(displayMediaOptions)
 
@@ -86,6 +103,8 @@ document.getElementById('screenShare').addEventListener('click',function(){
       }
 })
 });
+
+//this keeps our video/stream in a particular element id i.e in our video place or peers video place
 function receiveStream(stream,elemid){
 
     var video=document.getElementById(elemid);
@@ -95,12 +114,16 @@ function receiveStream(stream,elemid){
     window.peer_stream=stream;
 
 }
+
+//whenever a client reaches to the application just give him a random id so that he can get connected
 peer.on('open',function(){
 
     alert("Your id is "+peer.id+" .Use it to get connected");
 
 })
 
+//when pressed on connect button ,now you can chat to the peer but still can't see the video and can't show your screen
+//to him/her
 document.getElementById('connect').addEventListener('click',function(){
 
     peer_id=document.getElementById('connid').value;
@@ -109,6 +132,7 @@ document.getElementById('connect').addEventListener('click',function(){
 
 })
 
+//once a call is made one can video-chat/screen share/messages etc
 document.getElementById('call').addEventListener('click',function(){
 
     var call=peer.call(peer_id,window.localstream);
@@ -128,7 +152,7 @@ document.getElementById('call').addEventListener('click',function(){
 })
 
 
-
+//Procedures to be followed in client side after getting a request for connection from other client
 peer.on('connection',function(connection){
 
     conn=connection;
@@ -139,6 +163,7 @@ peer.on('connection',function(connection){
 
 })
 
+//Procedures to be followed in client side after getting a request for call from other client
 peer.on('call',function(call){
 
     call.answer(window.localstream);
@@ -152,25 +177,27 @@ peer.on('call',function(call){
 
 })
 
+//video on/off procedure
 videoOnOff.addEventListener('click',function(){
 
     let videoEnabled = myVideoStream.getVideoTracks()[0].enabled;
     if (videoEnabled) 
     {
-       videoOnOff.innerHTML='<img src="vidoff.jpg" width="40px" height="40px">';
+       videoOnOff.innerHTML='<img src="vidoff.png" width="40px" height="40px">';
 
        myVideoStream.getVideoTracks()[0].enabled = false;
 
     } 
     else
     {
-      videoOnOff.innerHTML='<img src="vid.png" width="40px" height="40px">';
+      videoOnOff.innerHTML='<img src="vid.jpg" width="40px" height="40px">';
 
       myVideoStream.getVideoTracks()[0].enabled = true;
 
     }
 })
 
+//audio on/off procedure
 audioOnOff.addEventListener('click',function(){
 
     let audioEnabled = myVideoStream.getAudioTracks()[0].enabled;
--- server.js
+++ server.js
@@ -1,20 +1,30 @@
-var express =require ('express')
-var app = express(); 
-var server = require('http').Server(app); 
-var io= require('socket.io')(server);
-var port = process.env.PORT || 3000
-app.use(express.static('public'))
-app.set('view engine','ejs');
+var express =require ('express'); //Requiring Express
 
-app.get('/', (req, res) => {
+var app = express(); //Initializing an express app
+
+var server = require('http').Server(app); //the server to listen for
+
+var io= require('socket.io')(server); //initializing socket.io
+
+var port = process.env.PORT || 3000; //The port on which our app will be listening upon
+
+app.use(express.static('public')); //Directory to look for while checking images/styling css files
+
+app.set('view engine','ejs'); //Look for ejs files when asked to render a file and these will be in views directory
+
+//hitting on / route our application calls various functionalities of this application
+app.get('/', function(req, res){
     res.render('index');
 });
 
+
+//Server getting a message from client side and emitting it in such a way that it reaches to all except the sender
 io.on('connection', function(socket){
     socket.on('chat', function(data){
-        socket.broadcast.emit('chat', data)
+        socket.broadcast.emit('chat', data);
     });
 
 });
 
+//listening to initialized port number
 server.listen(port);
